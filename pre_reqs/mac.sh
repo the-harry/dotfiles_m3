402615232462a9c227ab7967a6b50b1dd8aeb9d4
the-harry <matheusthebr@gmail.com>
2022-09-27 16:45:18 +0100

    Added ngrok to prereqs.

--- pre_reqs/mac.sh
+++ pre_reqs/mac.sh
@@ -5,7 +5,10 @@ brew tap homebrew/core
 
 git -C $(brew --repo homebrew/core) checkout master
 
-arch -x86_64 brew install zsh zsh-syntax-highlighting ctags tmux reattach-to-user-namespace the_silver_searcher fzf macvim mysql shared-mime-info gpg imagemagick watchman jq #poppler
+arch -x86_64 brew install zsh zsh-syntax-highlighting ctags tmux \
+             reattach-to-user-namespace the_silver_searcher fzf macvim \
+             mysql shared-mime-info gpg imagemagick watchman jq \
+             ngrok/ngrok/ngrok #poppler
 $(brew --prefix)/opt/fzf/install
 
 brew tap homebrew/cask-fonts
